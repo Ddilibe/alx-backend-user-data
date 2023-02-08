d79bedc62d1c888650b9d3cbfdc6555dc68932f6
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-02-08 21:08:57 +0100

    [Chore]: Done with task 1 and 2
    Empty Session and Create A Session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python3
 """ Script containing session authtication script """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
-    """ Class for creating a new authentication mechanism """
+    """ Class for creating a new authentication mechanism
+        Class Vari:
+            user_id_by_session_id - Class attribute initialized
+            by an empty dictionary
+    """
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        pass
+        """
+            Instance method that creates a session ID for a user_id
+            Args:
+                :params: user_id [str] - User id for the first argument
+            Return:
+        """
+        if user_id:
+            if isinstance(user_id, str):
+                key = str(uuid4())
+                SessionAuth.user_id_by_session_id[key] = user_id
+                return key
+        return None
