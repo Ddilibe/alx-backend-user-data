ffdf5cc9ff765412b51b956ce41fdf0093e8b87e
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-02-19 08:38:51 +0100

    [Chore]: Done with task 6

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""  Script to host a flask application """
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=True)
+def index() -> jsonify:
+	info = {
+		"message": "Bienvenue"
+	}
+	return jsonify(info)
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", port="5000")
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,14 +6,14 @@ import bcrypt
 
 
 def _hash_password(password: str) -> bcrypt.hashpw:
-	"""
-		Function that takes in a string and returns bytes
-		Args:
-			:params: @password - string first argument
-		Return:
-			Returns bytes
-	"""
-	return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    """
+        Function that takes in a string and returns bytes
+        Args:
+            :params: @password - string first argument
+        Return:
+            Returns bytes
+    """
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
 
 class Auth:
@@ -24,23 +24,23 @@ class Auth:
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-    	"""
-    		Method that registers a user object
-    		Args:
-    			:params: @email - Email of the to be registered user
-    			:params: @password - Password of the to registered user
-    		Return:
-    			Returns a user object
-    	"""
-    	new_user = {
-    		"email": email,
-    		"password": password
-    	}
-    	try:
-    		self._db.find_user_by(**new_user)
-    	except Exception as e:
-    		new_password = _hash_password(password)
-    		user = self._db.add_user(email, password)
-    		return user
-    	info = "User {} already exists".format(email)
-    	raise ValueError(info)
+        """
+            Method that registers a user object
+            Args:
+                :params: @email - Email of the to be registered user
+                :params: @password - Password of the to registered user
+            Return:
+                Returns a user object
+        """
+        new_user = {
+            "email": email,
+            "password": password
+        }
+        try:
+            self._db.find_user_by(**new_user)
+        except Exception as e:
+            new_password = _hash_password(password)
+            user = self._db.add_user(email, password)
+            return user
+        info = "User {} already exists".format(email)
+        raise ValueError(info)
