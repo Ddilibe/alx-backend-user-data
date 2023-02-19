ee02b3cf96c15310e1876b0f789750ee54ccec6e
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-02-19 08:39:58 +0100

    [Fix]: Fixed the app.py file

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,12 +5,14 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=True)
 def index() -> jsonify:
-	info = {
-		"message": "Bienvenue"
-	}
-	return jsonify(info)
+    info = {
+        "message": "Bienvenue"
+    }
+    return jsonify(info)
+
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5000")
